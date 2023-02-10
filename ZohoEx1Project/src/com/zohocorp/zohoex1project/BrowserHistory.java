ea9cca15718c81372fe4affa0e8a955b4e546b95
nirmalajoseph00 <nirmalajoseph002@gmail.com>
2023-02-10 16:46:01 +0530

    Initial Commit Signed-off-by: nirmalajoseph00 <nirmalajoseph002@gmail.com>

---
+++ ZohoEx1Project/src/com/zohocorp/zohoex1project/BrowserHistory.java
@@ -0,0 +1,11 @@
+package com.zohocorp.zohoex1project;
+
+public class BrowserHistory {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("hi");
+
+	}
+
+}
